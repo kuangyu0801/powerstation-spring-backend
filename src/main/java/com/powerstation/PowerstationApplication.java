bea2d0d5757df01205d12ad4b8f1ac39230eb32f
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-08-11 20:23:06 +0200

    Starter file with Spring Boot

---
+++ src/main/java/com/powerstation/PowerstationApplication.java
@@ -0,0 +1,13 @@
+package com.powerstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerstationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PowerstationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/powerstation/PowerstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerstationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
