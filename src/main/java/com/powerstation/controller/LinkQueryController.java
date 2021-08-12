3fce9e729bad20e810d725bb2e78d36c3c5cc775
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-08-13 00:53:10 +0200

    Add cross-origin support from angular-front-end

--- src/main/java/com/powerstation/controller/LinkQueryController.java
+++ src/main/java/com/powerstation/controller/LinkQueryController.java
@@ -4,11 +4,9 @@ import com.powerstation.dto.LinkQuery;
 import com.powerstation.dto.LinkQueryResponse;
 import com.powerstation.service.LinkQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("http://localhost:4200/")
 @RestController
 @RequestMapping("api/link-query")
 public class LinkQueryController {
--- src/main/java/com/powerstation/dao/StationRepository.java
+++ src/main/java/com/powerstation/dao/StationRepository.java
@@ -2,6 +2,8 @@ package com.powerstation.dao;
 
 import com.powerstation.entity.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200/")
 public interface StationRepository extends JpaRepository<Station, Integer> {
 }
